22dfd3f8e986843b1d8d2f43e0acc5098506bd06
Maimoona288 <maimoonashahbaz348@gmail.com>
2026-05-08 15:25:35 +1200

    comments and console cleaning

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,17 +1,3 @@
-// //ProtectedRoute
-// import { Navigate } from "react-router-dom";
-
-// const ProtectedRoute = ({ children }) => {
-//   const token = localStorage.getItem("token");
-
-//   if (!token) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
